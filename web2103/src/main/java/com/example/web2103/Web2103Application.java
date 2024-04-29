42388304a38fe4eed6c70f5c5e55cf1b32dbb268
78harris <167111808+78harris@users.noreply.github.com>
2024-04-29 15:11:32 +0800

    Add files via upload

---
+++ web2103/src/main/java/com/example/web2103/Web2103Application.java
@@ -0,0 +1,13 @@
+package com.example.web2103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web2103Application.class, args);
+    }
+
+}
---
+++ web2103/src/test/java/com/example/web2103/Web2103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2103ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

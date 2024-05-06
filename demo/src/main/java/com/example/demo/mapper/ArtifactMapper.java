1a2ebddce2ecade2bb4ada6e5a5eb5f7c31ffa1c
hby <1227138186@qq.com>
2024-05-06 23:01:11 +0800

    3.0 version

--- demo/src/main/java/com/example/demo/mapper/ArtifactMapper.java
+++ demo/src/main/java/com/example/demo/mapper/ArtifactMapper.java
@@ -11,25 +11,25 @@ import java.util.*;
 @Mapper
 public interface ArtifactMapper {
 
-    @Select("Select * from artifact where artifactname like CONCAT('%', #{artifactname}, '%') or artifactName_Chinese like CONCAT('%', #{artifactname}, '%') ")
+    @Select("Select id,imageUrl, artifactName_Chinese, size_Chinese, description_Chinese from artifact where artifactName like CONCAT('%', #{artifactname}, '%') or artifactName_Chinese like CONCAT('%', #{artifactname}, '%')")
     public List<Artifact> findByArtifactName(String artifactname);
 
     public List<Artifact> DeepSearch(String s);
 
 
-    @Select("Select * from artifact where library like CONCAT('%',#{libraryName},'%') or library_Chinese like CONCAT('%',#{libraryName},'%') ")
+    @Select("Select id,imageUrl, artifactName_Chinese, size_Chinese, description_Chinese from artifact where library like CONCAT('%',#{libraryName},'%') or library_Chinese like CONCAT('%',#{libraryName},'%') ")
     public List<Artifact> findByLibraryName(String libraryName);
 
-    @Select("Select * from artifact where relicTime like CONCAT('%',#{relicTime},'%')")
+    @Select("Select id,imageUrl, artifactName_Chinese, size_Chinese, description_Chinese from artifact where relicTime like CONCAT('%',#{relicTime},'%')")
     public List<Artifact> findByRelicTime(String relicTime);
 
-    @Select("select * from artifact where id=#{artifactId}")
+    @Select("select id,imageUrl, artifactName_Chinese, size_Chinese, description_Chinese from artifact where id=#{artifactId}")
     public Artifact findById(Integer artifactId);
 
-    @Select("SELECT * FROM artifact ")
+    @Select("SELECT id,imageUrl, artifactName_Chinese, size_Chinese, description_Chinese FROM artifact ")
     public List<Artifact> findAllRandom();
 
-    @Select("select * from artifact where " +
+    @Select("select id,imageUrl, artifactName_Chinese, size_Chinese, description_Chinese from artifact where " +
             "material_Chinese like CONCAT('%',#{materialChinese},'%') " +
             "or country =#{country} or library_Chinese=#{libraryChinese} " +
             "or relicTime_chinese=#{libraryChinese} " +
